3351d335de749bbe23950689e57621621f030ce1
Davud <davud.aliyev2007@gmail.com>
2024-02-08 15:46:13 +0400

    Add project files.

---
+++ Homework 28/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.IO;
+
+DirectoryInfo dirInfo = new DirectoryInfo("C:\\Users\\Asus\\Desktop\\C# Homeworks\\Homework 28");
+
+Console.WriteLine("DirInfo: ");
+Console.WriteLine("Fullname: "+ dirInfo.FullName);
+Console.WriteLine("Name: "+ dirInfo.Name);
+Console.WriteLine("Creation time: "+ dirInfo.CreationTime);
+Console.WriteLine("Last Access time: "+ dirInfo.LastAccessTime);
+Console.WriteLine("Parent: "+dirInfo.Parent);
+Console.WriteLine("Files count: "+dirInfo.GetFiles().Length);
+Console.WriteLine("Directories count: "+dirInfo.GetDirectories().Length);
+
+Console.WriteLine("\n================================================\n");
+
+
+FileInfo fileInfo = new FileInfo("C:\\Users\\Asus\\Desktop\\fileinfo");
+
+Console.WriteLine("fileinfo: ");
+Console.WriteLine("Fullname: "+fileInfo.FullName);
+Console.WriteLine("Name: "+fileInfo.Name);
+Console.WriteLine("Creation time: "+fileInfo.CreationTime);
+Console.WriteLine("Last access time: "+fileInfo.LastAccessTime);
+//Console.WriteLine("Length: " + fileInfo.Length);
+
+Console.WriteLine("\n================================================\n");
+
+FileInfo fileinfo1=new FileInfo("C:\\Users\\Asus\\Desktop\\fileinfo1");
+
+if(!fileinfo1.Exists)
+{
+    Console.WriteLine("fayl tapilmadi, yenisi yaradilsin? y/n");
+    string opt=Console.ReadLine();
+
+    if(opt=="y")
+    {
+        var newfs=fileinfo1.Create();
+        newfs.Close();
+    }
+}
+
+Console.WriteLine("Directories in files folder");
+
+foreach (var item in dirInfo.GetFiles())
+{
+    if(item.Length==0)
+    {
+        File.Delete(item.FullName);
+    }
+}
+
+
+
+
---
+++ LessonPractice/Program.cs
@@ -0,0 +1,86 @@
+﻿using LessonPractice;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+
+User user1 = null;
+user1 = new User()
+{
+    Name = "Raul",
+    Surname = "Aliyev",
+    Age = 23
+};
+
+Console.WriteLine(user1);
+
+void SerializeBinary(User user)
+{
+    FileStream fs=new FileStream("C:\\Users\\Asus\\Desktop\\User",FileMode.Create);
+    BinaryFormatter bf=new BinaryFormatter();
+    bf.Serialize(fs, user);
+    fs.Close();
+}
+
+User DeserializeBinary()
+{
+    FileStream fs=new FileStream("C:\\Users\\Asus\\Desktop\\User", FileMode.Open);
+    BinaryFormatter bf=new BinaryFormatter();
+    var data = bf.Deserialize(fs) as User;
+    return data;
+}
+
+void SerializeXml(User user)
+{
+    FileStream fs = new FileStream("C:\\Users\\Asus\\Desktop\\User2", FileMode.Create);
+    XmlSerializer bf=new XmlSerializer(typeof(User));
+    bf.Serialize(fs, user);
+    fs.Close();
+}
+
+User DeserializeXml()
+{
+    User data = null;
+
+    using (var fs = new FileStream("C:\\Users\\Asus\\Desktop\\User2", FileMode.Open)) 
+    {
+        XmlSerializer bf=new XmlSerializer(typeof(User));
+        data= bf.Deserialize(fs) as User;
+    }
+
+    return data;
+
+}
+
+void SerializeJson(User user)
+{
+    using(FileStream fs = new FileStream("C:\\Users\\Asus\\Desktop\\User3", FileMode.Create))
+    {
+        var jsonStr=JsonConvert.SerializeObject(user);
+        Console.WriteLine(jsonStr);
+
+        StreamWriter sw=new StreamWriter(fs);
+        sw.Write(jsonStr);
+        sw.Close();
+
+    }
+}
+
+User DeserializeJson()
+{
+    User data=null; 
+
+
+    using(FileStream fs=new FileStream("C:\\Users\\Asus\\Desktop\\User3", FileMode.Open))
+    {
+        StreamReader sr=new StreamReader(fs);
+        var jsonStr=sr.ReadToEnd(); 
+        data=JsonConvert.DeserializeObject<User>(jsonStr);
+    }
+
+    return data;
+}
+
+
+
